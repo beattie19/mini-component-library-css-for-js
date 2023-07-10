9216d915c9f8452dc1f49c3988934d45c056673a
Stephen Beattie <beattie19@hotmail.co.uk>
2023-07-10 14:49:32 +0200

    My unfinished initial attempt (working without visuals from figma so was making assumptions)

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,26 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const Input = styled.input`
+  width: var(--width)
+`
+
+const InputWrapper = styled.div`
+  
+`
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
   placeholder,
-}) => {
-  return 'TODO';
-};
+}) =>  
+  <InputWrapper>
+    <Input label = {label} placeholder = { placeholder}  type={'search'} style={{
+    '--width': width + 'px'
+  }}/>
+    <Icon id={icon}/>
+  </InputWrapper>;
+;
 
 export default IconInput;
